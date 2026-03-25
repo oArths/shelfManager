a4ad61e50609367d28156143641d932a02ae274d
Arthur Nascimento <arthur.nascimento@kantar.com>
2026-03-24 22:37:13 -0300

    Feat: update basename=/shelf-api

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -7,7 +7,7 @@ import { AuthProvider } from './contexts/AuthContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/shelf-api">
       <AuthProvider>
         <App />
       </AuthProvider>
